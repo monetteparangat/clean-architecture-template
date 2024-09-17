07ec50f19905ed493b81dfa24dafd2c5099101e9
MonetteParangat <monette.parangat@ibm.com>
2024-09-17 16:21:43 +0800

    Inital comit for CleanArchitecture.IoC - Adding dependencyContainer file

---
+++ CleanArchitectureSln/CleanArchitecture.Infrastructure.IoC/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.Services;
+using CleanArchitecture.Domain.Interfaces;
+using CleanArchitecture.Infrastructure.Data.Repository;
+
+namespace CleanArchitecture.Infrastructure.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Application layer
+            services.AddScoped<ICourseService, CourseService>();
+
+            //Infrastructure.Data layer
+            services.AddScoped<ICourseRepository, CourseRepository>();
+
+        }
+
+       
+    }
+}
