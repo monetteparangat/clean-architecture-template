f39f1c255bfa2a0230751d9e9019a9b3b3728855
MonetteParangat <monette.parangat@ibm.com>
2024-09-17 08:55:11 +0800

    AddDbContext on MVC and migrate UniversityDbContext

--- CleanArchitectureSln/CleanArchitecture.MVC/Program.cs
+++ CleanArchitectureSln/CleanArchitecture.MVC/Program.cs
@@ -1,3 +1,4 @@
+using CleanArchitecture.Infrastructure.Data.Context;
 using CleanArchitecture.MVC.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -6,12 +7,23 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("UniversityIdentityDBConnection") ?? throw new InvalidOperationException("Connection string 'UniversityIdentityDBConnection' not found.");
+var universityDbConnectionString = builder.Configuration.GetConnectionString("UniversityDbConnection") ?? throw new InvalidOperationException("Connection string 'UniversityDBConnection' not found."); ;
+
+// AddDbContext
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
+builder.Services.AddDbContext<UniversityDBContext>(options =>
+{
+    options.UseMySql(universityDbConnectionString, ServerVersion.AutoDetect(connectionString));
+});
+
+
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
+
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
