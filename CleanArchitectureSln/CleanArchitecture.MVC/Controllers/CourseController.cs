84797a555be1f8457544e2e87db6e481c3a0a737
MonetteParangat <monette.parangat@ibm.com>
2024-09-17 18:52:00 +0800

    Added controller, view, css, and image for Courses

---
+++ CleanArchitectureSln/CleanArchitecture.MVC/Controllers/CourseController.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using CleanArchitecture.Application.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.MVC.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ICourseService _courseService;
+
+        public CourseController(ICourseService courseService)
+        {
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            CourseVM model = _courseService.GetCourses();
+
+            return View(model);
+        }
+
+    }
+}
